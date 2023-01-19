8db670ddc3176c218cfcc3ed49450d7fe05e5aee
Bright Tenalo <debrightess@yahoo.com>
2023-01-19 22:25:22 +0100

    update product details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Helmet from '../components/Helmet/Helmet'
 import '../styles/home.css'
 
 import { Container, Row, Col } from 'reactstrap'
-import heroImg from '../assets/images/hero-img.png'
+import heroImg from '../assets/images/play-station.jpg'
 
 import Services from '../services/Services'
 import ProductsList from '../components/UI/ProductsList'
@@ -62,7 +62,7 @@ const Home = () => {
             <Col lg='6' md='6'>
               <div className='hero__content'>
                 <p className='hero__subtitle'>Trending product in {year} </p>
-                <h2>Make Your Interior More Minimalistic & Modern </h2>
+                <h2>Experience an all-new generation of incredible gadgets </h2>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Maxime eos exercitationem voluptate? Dignissimos quas velit
