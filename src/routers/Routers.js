6f3b9d933f53433e9f4af3883b0f3b8ec6eed1cb
Bright Tenalo <debrightess@yahoo.com>
2023-01-20 23:28:48 +0100

    add checkout to path

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -16,6 +16,7 @@ const Routers = () => {
       <Route path='shop' element={<Shop />} />
       <Route path='shop/:id' element={<ProductDetails />} />
       <Route path='cart' element={<Cart />} />
+      <Route path='checkout' element={<Checkout />} />
       <Route path='login' element={<Login />} />
       <Route path='signup' element={<Signup />} />
     </Routes>
