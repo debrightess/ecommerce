4c7b32045440b61bf4edeb9fa35b29a12afbaa32
Bright Tenalo <debrightess@yahoo.com>
2023-01-21 08:14:15 +0100

    update login and signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap'
+
+import Helmet from '../components/Helmet/Helmet'
+import '../styles/signup.css'
 
 const Signup = () => {
-  return <div>Signup</div>
+  const [file, setFile] = useState(null)
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <Helmet title='Signup'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' className='m-auto text-center'>
+              <h3 className='fw-bold mb-4'>Signup</h3>
+
+              <Form className='auth__form'>
+                <FormGroup className='form__group'>
+                  <input
+                    type='text'
+                    placeholder='Username'
+                    value={username}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <input
+                    type='email'
+                    placeholder='Enter your email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <input
+                    type='password'
+                    placeholder='Enter your password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className='form__group'>
+                  <label htmlFor='profile'>Choose a profile picture</label>
+                  <input
+                    type='file'
+                    id='profile'
+                    name='profile'
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </FormGroup>
+
+                <button type='submit' className='buy__btn auth__btn'>
+                  Create an Account
+                </button>
+                <p>
+                  Already have an account <Link to='/login'>Login</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  )
 }
 
 export default Signup
